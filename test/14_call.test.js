7a3e61d2d5f74c950c98e713cbc2926cf931f4aa
charishuang <charishuang@tencent.com>
2022-05-05 17:54:42 +0800

    update: test scripts

--- test/14_call.test.js
+++ test/14_call.test.js
@@ -2,11 +2,17 @@ import myCall from '../src/14_call';
 jest.setTimeout(60000);
 describe('call', () => {
   it('call', async () => {
-    const me = { name: 'Jack' }
+    const me = {
+      name: 'Jack',
+      say() {
+        return 'hello';
+      }
+    };
     function say() {
       return this.name || 'default';
     }
 
     expect(say.myCall(me)).toEqual('Jack');
+    expect(me.say()).toEqual('hello'); // 原方法不能被覆盖
   });
 });
