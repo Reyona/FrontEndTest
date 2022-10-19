9659b7b300643914062d10601cdd9785cb7e61e8
reyona <nokoru_ryohei_00@163.com>
2022-10-20 02:43:07 +0800

    更新答案记录为一个更易读的版本

--- _src/06_fetchWithRetry.js
+++ _src/06_fetchWithRetry.js
@@ -2,22 +2,20 @@ const MaxRetryCount = 3;
 const Timeout = 500;
 
 // 实现一个能自动重试的fetch
-export function fetchWithRetry(fetchPromise) {
-  let count = 0;
-  function doFetch() {
-    return Promise.race([
-      fetchPromise,
-      new Promise((resolve, reject) => {
-        setTimeout(()=> {
-          reject(new Error('request timeout'));
-        }, Timeout)})
-    ]).catch(error => {
-      console.error(error);
-      if (count < MaxRetryCount) {
-        count++;
-        return doFetch();
-      } else throw new Error('retry 3 times');
-    });
-  }
-  return doFetch();
+export function fetchWithRetry(fetchPromise, retryTime = MaxRetryCount) {
+  // 超时检测
+  const timeoutP = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      reject(new Error('timeout'))
+    }, Timeout);
+  });
+  return Promise.race([timeoutP, fetchPromise]).then(result => {
+    return result;
+  }).catch(error => {
+    if (retryTime) {
+      return fetchWithRetry(fetchPromise, retryTime - 1);
+    } else {
+      throw new Error('retry 3 times');
+    }
+  })
 }
